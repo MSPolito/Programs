8c3affbd1dc395f9bfee41c1be010bcec32a0d6e
Josh <Josh@Mjbrowns.com>
2019-06-17 20:45:16 -0400

    Update yuh.py

--- yuh.py
+++ yuh.py
@@ -1,3 +1,4 @@
 def ratios(calories, carb = .4, protein = .3, fat = .3):
         return [round(carb*calories/4), round(protein*calories/4), round(fat*calories/9)]
 print(ratios(0.3,0.3,2000))
+#yuh
