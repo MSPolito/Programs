f4a86bc979ee4849ffc99891932230f1eeb2789a
MSPolito <44487827+MSPolito@users.noreply.github.com>
2019-06-18 20:07:39 -0400

    Update to pictures

--- Prj3_Files/Project_3.py
+++ Prj3_Files/Project_3.py
@@ -1,31 +1,35 @@
 import nltk #this will not work if you haven't installed nltk
 
-def extract_tweets('test_file.txt', 'r', how = 'user'):
+def extract_tweets(tweetfile, party, how = 'user'):
     #get the tweets out of the file and put them in a dictionary based on
     #a characteristic
-    result = []
-    with open('test_file.txt','r') as file:
+    result = {}
+    with open(tweetfile,'r') as file:
         for line in file:
-            data = line.split()
-            tweet = process_tweet(data[2])
+            line = process_tweet(line)
+            data = {'user': -3, 'party': -2, 'state': -1}
             if data[how] in result:
-                result[data[how]] = tweet
+                result[data[how]].append(line)
             else:
-                result[data[how]].append(tweet)
-            return result
+                result[data[how]] = line
+        return result
 #above code is unfinished
 def process_tweet(tweet):
-  tw = itw.lower().translate('%amp', '    ').maketrans('', '', ',!?.+-$%^Z&*()_|')
-  return tw
-print(tw)
+    trantab = maketrans('&amp', 'and')
+    tweet = tweet.lower().translate(trantab)
+    specialch = ',!?.+-$%^Z&*()_|'
+    for i in specialch:
+        tweet.replace(i , '')
+    return tweet
     # pass
 
 def compare_hashes(tweets, level = 10):
   hashtags = []
   for word in test_file:
-    state =  testfile[-2:]
+    state =  testfile[-1:]
     srtlst = state.sort()
     hstg = srtlst.find('#')
-
-print(new_text)
+    cthstg = hstg.count()
+    tweets = (cthstg, hstg)
+    return(tweets)
 #     pass
--- Prj3_Files/Project_3_Main.py
+++ Prj3_Files/Project_3_Main.py
@@ -1,3 +1,9 @@
 from Project_3 import *
 import nltk
 import string
+
+tweets = extract_tweets('sen_tweet1_edited_2.csv', 'state')
+print(compare_hashes(tweets, level = 2))
+
+tweets = extract_tweets('sen_tweet1_edited_2.csv', 'party')
+print(compare_hashes(tweets, level = 15))
